c28dd1580a7ee9f0666b7066cbfe743c883e9c2e
konman01 <konimanjunatha@gmail.com>
2020-01-27 01:36:45 -0800

    Adding JavaScript to the project

---
+++ CSS/Course-Project/shared.js
@@ -0,0 +1,35 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var noOption = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+for (var i = 0; i < selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function () {
+        
+        modal.style.display = 'block';
+        backdrop.style.display = 'block';
+
+    });
+
+}
+
+backdrop.addEventListener('click', function () {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+noOption.addEventListener('click', closeModal);
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+
+
+toggleButton.addEventListener('click', function () {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
