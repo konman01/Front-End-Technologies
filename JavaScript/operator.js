29ec67d54b56270fda0f15a52b3677cc0f11ec35
konman01 <konimanjunatha@gmail.com>
2020-01-30 00:35:03 -0800

    operators in JS

---
+++ JavaScript/operator.js
@@ -0,0 +1,19 @@
+ /*number + number = number
+ string + string = string // concatination
+ string + number = string
+ number + string = string
+ true + string = 'true string'
+ [1, 2] + string = '1, 2 string
+ 12 + true = 13
+ true + true = 2
+ 12 + null = 12
+ 12 + undefined = NaN
+ 12 + NaN = NaN
+ 
+ string - string = NaN
+ number - '1' = number
+ number + false/true = number
+ 
+ 
+ flaot * number = number
+ number * null = 0*/
